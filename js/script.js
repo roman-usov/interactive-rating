068fb7f9e5c7673a0e80a1e4487f52477befa120
roman-usov <66104246+roman-usov@users.noreply.github.com>
2022-06-04 20:27:02 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const ratingFormEl = document.querySelector('.rating-form');
+const thankYouEl = document.querySelector('.thank-you-content');
+const ratingSubmitContainerEl = document.querySelector(
+  '.rate-submit-container'
+);
+const ratingButtonEls = document.querySelectorAll('.rate-btn');
+const selectedRatingEl = document.querySelector('.score');
+
+ratingSubmitContainerEl.addEventListener('click', (e) => {
+  const button = e.target;
+
+  if (!button) {
+    return;
+  }
+
+  if (button.matches('.rate-btn')) {
+    ratingButtonEls.forEach((btn) => {
+      btn.dataset.rating = '';
+    });
+    button.dataset.rating = button.value;
+  }
+});
+
+ratingFormEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const selectedRating = [...ratingButtonEls].find(
+    (btn) => btn.dataset.rating !== ''
+  );
+
+  if (!selectedRating) {
+    return;
+  }
+
+  selectedRatingEl.textContent = selectedRating.dataset.rating;
+
+  ratingFormEl.addEventListener(
+    'transitionend',
+    () => {
+      ratingFormEl.classList.add('hidden');
+      thankYouEl.classList.remove('hidden');
+      thankYouEl.classList.add('fade-in');
+    },
+    {
+      capture: false,
+      once: true,
+      passive: false,
+    }
+  );
+  ratingFormEl.classList.add('fade-out');
+});
